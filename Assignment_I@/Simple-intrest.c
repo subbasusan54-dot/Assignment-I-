fb0fb9523445a0227591c8bea9fa31ed9b3cccad
subbasusan54-dot <subba.susan54@gmail.com>
2026-02-05 21:03:26 +0545

    Add files via upload

---
+++ Assignment_I@/Simple-intrest.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+    float p, t, r, si;
+
+    printf("Enter principal, time, and rate:\n");
+    scanf("%f %f %f", &p, &t, &r);
+
+    si = (p*t*r)/100;
+
+    printf("Simple Interest is %.2f", si);
+    return 0;
+}
